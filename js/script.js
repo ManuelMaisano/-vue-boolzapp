a3399cb45a22fb06270e66b0b23ec751746380ca
Manuel Maisano <manumaisano@gmail.com>
2024-04-04 09:48:42 +0200

    div id app data and script

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+//Milestone 1
+//eplica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+//Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+
+import { contacts } from "./data.js";
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      contacts
+    }
+  },
+  methods: {},
+  computed: {},
+  mounted() {
+    console.log(this.contacts);
+  },
+}).mount("#app");
